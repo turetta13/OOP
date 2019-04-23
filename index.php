fd03b9ad2ddfc9476646616d25977deac8f1fb7c
turetta13 <turetta13@gmail.com>
2019-04-23 19:38:46 +0300

    fix

--- index.php
+++ index.php
@@ -7,16 +7,13 @@ $users = [['login' => 'Vasisualiy', 'password' => '12345', 'lang' => 'ru'],
     ['login' => 'Petro', 'password' => 'EkUC42nzmu', 'lang' => 'ua'],
     ['login' => 'Pedrolus', 'password' => 'Cogito_ergo_sum', 'lang' => 'it'],
     ['login' => 'Sasha', 'password' => 'Alea_est_jacta']];
-
 ?>
     <html>
     <body>
     <form method="POST">
         <input type="text" name="login">
         <input type="password" name="password">
-        <input type="text" name="lang">
         <input type="submit" value="login" name="">
-        <a href="lang.php">вперед</a>
     </form>
     </body>
     </html>
@@ -29,27 +26,24 @@ class Users
     public $password;
     public $lang;
 
-    public function __construct($login, $password, $lang = 'ru')
+    public function __construct($login, $password)
     {
         $this->login = $login;
         $this->password = $password;
-        $this->lang = $lang;
     }
 
     public function getUser()
     {
-        return "{$this->login} говорит на языке {$this->lang}";
+        return "{$this->login} говорит на языке ";
     }
 }
 
-$users1 = new Users($_POST['login'], $_POST['password'], $_POST['lang']);
-
+$users1 = new Users($_POST['login'], $_POST['password']);
 
 foreach ($users as $value) {
     if ($users1->login == $value['login'] && $users1->password == $value['password']) {
         $_SESSION['user'] = $value;
-        echo $users1->getUser();
+        echo $users1->getUser() . $value['lang'];
     }
 }
-
 ?>
